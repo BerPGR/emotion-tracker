9b9735b35acf321c3b35fd205f6aac7082b15054
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2025-04-04 11:09:58 -0300

    feat: added emotion type

---
+++ src/types/emotion.ts
@@ -0,0 +1,10 @@
+export type Emotion = "joy" | "sadness" | "anger" | "calm" | "fear" | "neutral";
+
+export const EmotionLabels: Record<Emotion, string> = {
+    joy: "Alegria",
+    sadness: "Tristeza",
+    anger: "Raiva",
+    calm: "Calma",
+    fear: "Medo",
+    neutral: "Neutro"
+}
