2173e69d4502cec08e8a72dbd0dcb108bdcf543f
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2025-04-04 11:07:51 -0300

    feat: added navbar to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import Navbar from "../components/layout/Navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-calm to-joy">
-      <h1 className="text-white text-5xl font-bold mb-6">Controle de Emoções</h1>
-      <Link to="/registrar" className="bg-white text-calm px-6 py-3 rounded-full text-lg font-medium shadow-lg">
-        Registrar Emoção
-      </Link>
-    </div>
+    <>
+      <Navbar />
+      <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-calm to-joy">
+        <h1 className="text-white text-5xl font-bold mb-6">Controle de Emoções</h1>
+        <Link to="/registrar" className="bg-white text-calm px-6 py-3 rounded-full text-lg font-medium shadow-lg">
+          Registrar Emoção
+        </Link>
+      </div>
+    </>
   );
 }
