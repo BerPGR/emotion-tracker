fd73cb0aaab7b8df36a919b335b20e070c12bd25
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2025-04-06 20:12:05 -0300

    feat: edit in home page and router

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-calm to-joy">
+      <div className="container mx-auto min-h-screen flex flex-col p-8 bg-gradient-to-br from-calm to-joy content-center">
         <h1 className="text-white text-5xl font-bold mb-6">Controle de Emoções</h1>
         <Link to="/registrar" className="bg-white text-calm px-6 py-3 rounded-full text-lg font-medium shadow-lg">
           Registrar Emoção
